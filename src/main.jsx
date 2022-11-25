0d17d135ee1aa38ec48199106e22dfa242a5f191
marstarek <tarekahmed1568@gmail.com>
2022-11-26 00:01:01 +0200

    router_dom

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,28 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
-
+import ErrorPage from "./components/ErrorPage/ErrorPage";
+import Home from "./components/Home/Home";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+} from "react-router-dom";
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: "/home",
+    element: <Home />,
+  },
+]);
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+
+      <RouterProvider router={router} />
+
   </React.StrictMode>
 )
